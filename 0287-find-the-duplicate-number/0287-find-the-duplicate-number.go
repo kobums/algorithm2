0ff4ad6cbda051f8516d6114c21b6339bb506756
고범석 <55486001+kobums@users.noreply.github.com>
2024-07-26 21:16:55 +0900

    Time: 77 ms (66.41%), Space: 7.1 MB (100%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
@@ -0,0 +1,13 @@
+func findDuplicate(nums []int) int {
+    slow, fast := nums[0], nums[nums[0]]
+	for slow != fast {
+		slow, fast = nums[slow], nums[nums[fast]]
+	}
+
+	slow = 0
+	for slow != fast {
+		slow, fast = nums[slow], nums[fast]
+	}
+
+	return slow
+}
