cca052d0aa944c1218c1f34fde76d0bfcba2a1c4
kobums <kobums@naver.com>
2026-01-18 13:29:31 +0900

    Time: 42 ms (52.92%), Space: 46.2 MB (48.54%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        // DP 배열 생성 및 1로 초기화
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        int maxLen = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                // 증가하는 수열 조건
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+}
